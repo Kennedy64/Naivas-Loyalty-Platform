0ff7333641b0870dbb080c3eabaf4f352d94bc64
Kennedy64 <wayne.kennedy@strathmore.edu>
2026-06-19 22:18:23 +0300

    Add contact form validation

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
-console.log("Naivas Loyalty App Loaded");
+document.addEventListener("DOMContentLoaded", function () {
+
+const form = document.getElementById("contactForm");
+
+if(form){
+
+form.addEventListener("submit", function(event){
+
+const name = document.getElementById("name").value.trim();
+const email = document.getElementById("email").value.trim();
+const message = document.getElementById("message").value.trim();
+
+if(name === "" || email === "" || message === ""){
+
+event.preventDefault();
+
+alert("Please fill in all fields.");
+
+}
+
+});
+
+}
+
+});
