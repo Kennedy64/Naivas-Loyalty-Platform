21202dcb7ade2500e7758ca5556fc2f6347d36d8
Kennedy64 <wayne.kennedy@strathmore.edu>
2026-06-21 14:56:32 +0300

    Final change on js

--- script.js
+++ script.js
@@ -23,10 +23,18 @@ document.addEventListener("DOMContentLoaded", function () {
 
 });
 
+function toggleDarkMode() {
+    document.body.classList.toggle("dark-mode");
 
+    if (document.body.classList.contains("dark-mode")) {
+        localStorage.setItem("theme", "dark");
+    } else {
+        localStorage.setItem("theme", "light");
+    }
+}
 
- function toggleDarkMode(){
-
-document.body.classList.toggle("dark-mode");
-
-} 
+window.onload = function () {
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+}; 
